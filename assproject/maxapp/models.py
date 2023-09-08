c2fa9a3431156ae27ae54904150c94c7cd5eb006
ASWANTH <142484784+aswanth0218@users.noreply.github.com>
2023-09-08 20:45:56 +0530

    Delete assproject directory
    delete assproject directory

--- assproject/maxapp/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class register(models.Model):
-    number1 = models.IntegerField
-    number2 = models.IntegerField
--- assproject/maxapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.register),
-    path('register/approvel',views.approvel),
-
-]
